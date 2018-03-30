459b83ad5ea4839ffb7e3adb2750cda362bf7793
Miguel Mota <miguelmota2@gmail.com>
2018-03-30 10:29:26 -0700

    exports

--- table/sort.go
+++ table/sort.go
@@ -1,14 +1,21 @@
 package table
 
+// SortOrder int
 type SortOrder int
+
+// SortFn sort function
 type SortFn func(interface{}, interface{}) bool
 
 const (
+	// SortNone sort none
 	SortNone SortOrder = iota
+	// SortAsc sort ascendinge
 	SortAsc
+	// SortDesc sort descending
 	SortDesc
 )
 
+// SortBy sort by
 type SortBy struct {
 	index  int
 	order  SortOrder
