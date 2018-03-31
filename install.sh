f446e71c7bd6bc1a7fff90bbbd57606282c2a52a
Miguel Mota <miguelmota2@gmail.com>
2018-03-31 01:29:19 -0700

    update script
    Former-commit-id: 3a8b32691c98484cb1c237fc8b3f8d99872b7fd0 [formerly 3a8b32691c98484cb1c237fc8b3f8d99872b7fd0 [formerly 5edfd66cf0412a31be55793fc345d194c193877d [formerly 10df42aa3c6c30cfb9eb10e2ed3dc3d00c29cf5f]]]
    Former-commit-id: d20de55f88aac6a4a5ce460d5bf46394fe863fd8
    Former-commit-id: 20f765d62ffd701a44b739a066399ac9b3c2875f [formerly 562fd3375a13674d7bab01bf02e313a41cf69598]
    Former-commit-id: 356959113ff76f8294dc7529936176deab47134d

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
+echo "downloading..."
 curl https://github.com/miguelmota/cointop/raw/master/bin/cointop -o cointop
+echo "installing..."
 mv cointop /usr/local/bin/
+echo "cointop installed."
