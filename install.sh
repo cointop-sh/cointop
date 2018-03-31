fa3c14e48dda9a1572bc27ce5cadc95462a1e836
Miguel Mota <miguelmota2@gmail.com>
2018-03-31 03:55:51 -0700

    update script

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 echo "downloading..."
 curl -s "https://github.com/miguelmota/cointop/raw/master/bin/cointop" -o cointop
 echo "installing..."
+chmod +x cointop
 mv cointop /usr/local/bin/
 echo "cointop installed."
