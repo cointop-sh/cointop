3c55749e3405f1831ba45d72de16175a73575775
Miguel Mota <hello@miguelmota.com>
2019-07-05 00:16:56 -0700

    v1.3.4

--- cointop/version.go
+++ cointop/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // TODO: make dynamic based on git tag
-const version = "1.3.3"
+const version = "1.3.4"
 
 // Version returns the cointop version
 func (ct *Cointop) Version() string {
