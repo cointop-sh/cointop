523c43025a413f8cfe667d940f5fba5d8dbc8bf3
Miguel Mota <miguelmota2@gmail.com>
2020-08-04 22:27:08 -0700

    Read version from runtime build info if available

--- cointop/version.go
+++ cointop/version.go
@@ -3,18 +3,27 @@ package cointop
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 )
 
 // version is the cointop version which will be populated by ldflags
-var version = "dev"
+var version string = "(unknown)"
 
 // Version returns the cointop version
 func (ct *Cointop) Version() string {
-	return version
+	return Version()
 }
 
 // Version returns cointop version
 func Version() string {
+	if version != "(unknown)" {
+		return version
+	}
+
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		return buildInfo.Main.Version
+	}
+
 	return version
 }
 
