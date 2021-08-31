0074ecfff99e8662f9e3d07614ee196aacb1d8b9
Simon Roberts <simon.roberts@anz.com>
2021-09-01 09:29:04 +1000

    Patch expression to substitute Float64 for Integer. Update docs to include '/'

--- pkg/eval/eval.go
+++ pkg/eval/eval.go
@@ -5,25 +5,37 @@ import (
 	"strings"
 
 	"github.com/antonmedv/expr"
+	"github.com/antonmedv/expr/ast"
 )
+
+// AST Visitor that changes Integer to Float
+type patcher struct{}
+
+func (p *patcher) Enter(_ *ast.Node) {}
+func (p *patcher) Exit(node *ast.Node) {
+	n, ok := (*node).(*ast.IntegerNode)
+	if ok {
+		ast.Patch(node, &ast.FloatNode{Value: float64(n.Value)})
+	}
+}
 
 // EvaluateExpression evaulates a simple math expression string to a float64
 func EvaluateExpressionToFloat64(input string) (float64, error) {
-	input = strings.TrimSpace(input) // remove trailing \0s
+	input = strings.TrimSpace(input)
 	if input == "" {
 		return 0, nil
 	}
-	result, err := expr.Eval(input, nil)
+	program, err := expr.Compile(input, expr.Env(nil), expr.Patch(&patcher{}))
+	if err != nil {
+		return 0, err
+	}
+	result, err := expr.Run(program, nil)
 	if err != nil {
 		return 0, err
 	}
 	f64, ok := result.(float64)
 	if !ok {
-		ival, ok := result.(int)
-		if !ok {
-			return 0, errors.New("could not type assert float64")
-		}
-		f64 = float64(ival)
+		return 0, errors.New("could not type assert float64")
 	}
 	return f64, nil
 }
