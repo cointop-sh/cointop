bbf4144ebb32b8d3d89defe80261e51acb9de253
Simon Roberts <lyricnz@gmail.com>
2021-10-09 12:39:42 +1100

    Fix edge case with resample min-time

--- pkg/timedata/timedata.go
+++ pkg/timedata/timedata.go
@@ -18,7 +18,11 @@ func ResampleTimeSeriesData(data [][]float64, start float64, end float64, numSte
 		idx := sort.Search(l, func(i int) bool { return data[i][0] >= pos })
 		var val float64
 		if idx == 0 {
-			val = math.NaN() // off the left
+			if data[0][0] == pos {
+				val = data[0][1] // exactly left
+			} else {
+				val = math.NaN() // off the left
+			}
 		} else if idx == l {
 			val = math.NaN() // off the right
 		} else {
