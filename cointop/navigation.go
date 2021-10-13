b19864014e8ccb2df318e9716d02f06f6e264c48
Simon Roberts <lyricnz@users.noreply.github.com>
2021-10-13 18:26:12 +1100

    After jumping to the right page, search for the right row (#220)

--- cointop/navigation.go
+++ cointop/navigation.go
@@ -409,13 +409,18 @@ func (ct *Cointop) GoToPageRowIndex(idx int) error {
 
 // GoToGlobalIndex navigates to the selected row index of all page rows
 func (ct *Cointop) GoToGlobalIndex(idx int) error {
-	log.Debug("GoToGlobalIndex()")
+	log.Debugf("GoToGlobalIndex(%d)", idx)
+	target := ct.State.allCoins[idx]
 	l := ct.TableRowsLen()
 	atpage := idx / l
 	ct.SetPage(atpage)
-	rowIndex := idx % l
-	ct.HighlightRow(rowIndex)
 	ct.UpdateTable()
+	// Look for the coin in the current page
+	for i, coin := range ct.State.coins {
+		if coin == target {
+			ct.HighlightRow(i)
+		}
+	}
 	return nil
 }
 
