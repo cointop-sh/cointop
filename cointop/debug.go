8c976d3c0f961170554f192e9b81c1600beef172
Simon Roberts <lyricnz@gmail.com>
2021-10-17 19:07:47 +1100

    If $DEBUG_FILE is set, use that rather than /tmp/cointop.log

--- cointop/debug.go
+++ cointop/debug.go
@@ -3,11 +3,16 @@ package cointop
 import (
 	"os"
 
+	"github.com/cointop-sh/cointop/pkg/pathutil"
 	log "github.com/sirupsen/logrus"
 )
 
 func (ct *Cointop) initlog() {
 	filename := "/tmp/cointop.log"
+	debugFile := os.Getenv("DEBUG_FILE")
+	if debugFile != "" {
+		filename = pathutil.NormalizePath(debugFile)
+	}
 	f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
 		panic(err)
